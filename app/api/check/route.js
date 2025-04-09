b51726f6a89b5ace32f150c1879f5869204ac9c5
Pranesh <pranesh25joshi@gmail.com>
2025-04-09 22:24:56 +0530

    Refactor username availability check to use fetch instead of axios

--- app/api/check/route.js
+++ app/api/check/route.js
@@ -1,35 +1,36 @@
-import axios from "axios";
 import sites from "@/app/sites";
 
 export async function POST(req) {
-  const { username } = await req.json();
-
-  if (!username)
-    return new Response(JSON.stringify({ error: "Username is required" }), {
-      status: 400,
-      headers: { "Content-Type": "application/json" },
-    });
-
-  const check = Object.entries(sites).map(async ([siteName, details]) => {
-    const url = details.url.replace("{}", username);
-
-    try {
-      const response = await axios.get(url);
-      if (response.status === 200 && response.data.includes(username)) {
-        return { siteName, available: true, url };
-      } else {
+    const { username } = await req.json();
+  
+    if (!username)
+      return new Response(JSON.stringify({ error: "Username is required" }), {
+        status: 400,
+        headers: { "Content-Type": "application/json" },
+      });
+  
+    const check = Object.entries(sites).map(async ([siteName, details]) => {
+      const url = details.url.replace("{}", username);
+  
+      try {
+        const response = await fetch(url);
+        const text = await response.text();
+        if (response.status === 200 && text.includes(username)) {
+          return { siteName, available: true, url };
+        } else {
+          return { siteName, available: false, url };
+        }
+      } catch (e) {
         return { siteName, available: false, url };
       }
-    } catch (e) {
-      return { siteName, available: false, url };
-    }
-  });
-
-  const results = await Promise.all(check);
-  const available = results.filter((result) => result.available);
-
-  return new Response(JSON.stringify({ available }), {
-    status: 200,
-    headers: { "Content-Type": "application/json" },
-  });
+    });
+  
+    const results = await Promise.all(check);
+    const available = results.filter((result) => result.available);
+  
+    return new Response(JSON.stringify({ available }), {
+      status: 200,
+      headers: { "Content-Type": "application/json" },
+    });
 }
+  
